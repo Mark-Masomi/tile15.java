67e9535b161694ca0b00d996475955a022e61b28
Moin93 <Moinmirmasomi@outlook.com>
2022-10-27 17:43:14 +0200

    smal changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        puzzel15 p15=new puzzel15();
 
     }
 }
--- src/com/company/puzzel15.java
+++ src/com/company/puzzel15.java
@@ -1,18 +1,28 @@
 package com.company;
-
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class puzzel15 extends JFrame implements ActionListener {
-    JFrame puzzel=new JFrame();
     JPanel gamePanel = new JPanel();
-    JButton[] buttons=new JButton[15];
-
+    JPanel resetPanel=new JPanel();
+    JButton nyttspel=new JButton("Start New Game");
+    //JButton emptybutton=new JButton("");
+    ArrayList<JButton> buttons_list=new ArrayList<>();
+    JButton[] buttons=new JButton[16];
+    public static JButton placeholder(JButton emptybutton){
+        return emptybutton;
+    }
     public puzzel15() {
-        puzzel.add(gamePanel);
+        setLayout(new BorderLayout());
+        add(gamePanel,"Center");
+        add(resetPanel,"North");
+        resetPanel.add(nyttspel);
+        //gamePanel.add(emptybutton);
         gamePanel.setLayout(new GridLayout(4,4));
         String nr="";
         for (int i = 0; i < buttons.length; i++) {
@@ -20,13 +30,48 @@ public class puzzel15 extends JFrame implements ActionListener {
             if (i != buttons.length - 1) {
                 buttons[i].setText(Integer.toString(i + 1));
             }
-            buttons[i].addActionListener(this);
+            //buttons[i].addActionListener(this);
+
             gamePanel.add(buttons[i]);
         }
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(500,500);
+        nyttspel.addActionListener(this);
+        buttons[15].setText("");
+        for (JButton buttons :buttons) {
+            buttons.addActionListener(this);
+        }
+        Collections.addAll(buttons_list,buttons);
+
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if ()
+        //int indxnr=0;
+
+        if (e.getSource()==nyttspel) {
+            Collections.shuffle(/*Arrays.asList(buttons)*/buttons_list);
+            for (JButton button : buttons_list/*buttons*/) {
+                gamePanel.add(button);
+            }
+            gamePanel.revalidate();
+        } /*else if (e.getSource()!=nyttspel) {
+            for (JButton b:buttons_list
+                 ) {
+                b.
+            }*/
+        } 
+        /*else if (e.getSource()!=buttons[16]){
+            Collections.swap(Arrays.asList(buttons),e.hashCode(),15);
+            for (JButton button : buttons) {
+                gamePanel.add(button);
+            }
+            gamePanel.revalidate();
+        }*/
+    //public int emptytile_index(ArrayList)
     }
-}
+
+
